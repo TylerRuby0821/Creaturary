fa89193d704487b18abaeb00b559853fa094284d
tyler <tylerruby0314@yahoo.com>
2021-05-05 11:23:52 -0500

    Starter landing styling and imports

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './LandingPage.css'
 
 const LandingPage = () => {
   return (
     <div className='landing__container'>
       <h1 className='landing__header'>Creaturary</h1>
-      <h3>All things spooky, mythical, magical and beyond!</h3>
-      <NavLink to='/login'> Login </NavLink>
-      <NavLink to='/sign-up'> Signup </NavLink>
+      <h3 className='landing__subheader'>All things spooky, mythical, magical and beyond...</h3>
+      <div className='landing__nav--container'>
+        <NavLink className='landing__login--nav' to='/login'> Login </NavLink>
+        <NavLink className='landing__signup--nav' to='/sign-up'> Signup </NavLink>
+      </div>
     </div>
   )
 }
