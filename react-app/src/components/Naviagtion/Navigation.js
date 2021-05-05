5458d4ca06655f18f33bf1f18304ecbc3efa9599
tyler <tylerruby0314@yahoo.com>
2021-05-05 18:26:18 -0500

    Basic navigation styling done, Need to add usermenu so user can click and sign out

--- react-app/src/components/Naviagtion/Navigation.js
+++ react-app/src/components/Naviagtion/Navigation.js
@@ -6,7 +6,7 @@ import UserMenu from '../UserMenu/UserMenu'
 const Navigation = () => {
 
   const user = useSelector(state => state.session.user)
-  console.log(user)
+
   return (
     <div className='navbar__container'>
       <div className='navbar__header--container'>
@@ -17,7 +17,9 @@ const Navigation = () => {
       </div>
       <div className='navbar__user--container'>
         <h3 className='navbar__username'>{user.username}</h3>
-        <div><i class="fas fa-cog"></i></div>
+        <span className='navbar__usermenu'>
+          <i class="fas fa-bars"></i>
+        </span>
       </div>
     </div>
 
