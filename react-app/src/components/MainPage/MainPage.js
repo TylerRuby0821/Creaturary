15c1a91c018f2b06fe92d8ef1dcd2a85f32d8779
tyler <tylerruby0314@yahoo.com>
2021-05-06 10:31:09 -0500

    Added Books for navigation, positioned thier text, minor styling

--- react-app/src/components/MainPage/MainPage.js
+++ react-app/src/components/MainPage/MainPage.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 import Navigation from '../Naviagtion/Navigation'
+import './MainPage.css'
 
 const MainPage = () => {
 
-  return (
-    <div className='main__container'>
-      <Navigation />
-      <div className='main__body'>
-        <div>A - Z</div>
-        <div> Lore </div>
-        <div> Custom</div>
-      </div>
-    </div>
+  const user = useSelector(state => state.session.user)
 
+  return (
+    <div>
+      {(user) ?
+        <div className='main__container'>
+          <Navigation />
+          <div className='main__body'>
+            <div className='main__body--lore'>
+              <i class="fas fa-book"></i>
+              <div className='book__text'>Lore</div>
+            </div>
+            <div className='main__body--az'>
+              <i class="fas fa-book"></i>
+              <div className='book__text'>A-Z</div>
+            </div>
+            <div className='main__body--custom'>
+              <i class="fas fa-book"></i>
+              <div className='book__text'>Custom</div>
+            </div>
+          </div>
+        </div>
+        : Redirect('/')}
+    </div>
   )
 }
 
