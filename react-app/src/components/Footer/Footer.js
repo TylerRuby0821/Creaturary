6f54dfe177153440169bf7fce677b1383087412f
tyler <tylerruby0314@yahoo.com>
2021-05-25 12:25:42 -0500

    Fixed images not loading on heroku, fixed footer bar positioning

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Footer.css'
+import github from './GitHub-Mark.png'
+import linkedIn from './LI-In-Bug.png'
 
 const Footer = () => {
   return (
     <div className='footer__container'>
       <a className='footer__link' href='https://github.com/TylerRuby0821'>
-        <img className='footer__image' src= '/GitHub-Mark.png' alt='GitHub' height={50} width={50}></img>
+        <img className='footer__image' src= {github} alt='GitHub' height={50} width={50}></img>
       </a>
       <a className='footer__link' href='https://www.linkedin.com/in/tyler-ruby-b700161ba/'>
-        <img className='footer__image' src= '/LI-In-Bug.png' alt='LinkedIn' height={50} width={50}></img>
+        <img className='footer__image' src= {linkedIn} alt='LinkedIn' height={50} width={50}></img>
       </a>
       {/* <a className='footer__link' href=''> */}
         {/* <img className='footer__link' src='' alt='' height={50} width={50}></img> */}
