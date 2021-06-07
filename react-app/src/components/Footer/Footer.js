7f52395020e7453d5ea855b437b1ca2b4dbf49e4
tyler <tylerruby0314@yahoo.com>
2021-06-07 13:25:01 -0500

    Added portfolio link to footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -6,15 +6,21 @@ import linkedIn from './LI-In-Bug.png'
 const Footer = () => {
   return (
     <div className='footer__container'>
-      <a className='footer__link' href='https://github.com/TylerRuby0821'>
-        <img className='footer__image' src= {github} alt='GitHub' height={50} width={50}></img>
-      </a>
-      <a className='footer__link' href='https://www.linkedin.com/in/tyler-ruby-b700161ba/'>
-        <img className='footer__image' src= {linkedIn} alt='LinkedIn' height={50} width={50}></img>
-      </a>
-      {/* <a className='footer__link' href=''> */}
-        {/* <img className='footer__link' src='' alt='' height={50} width={50}></img> */}
-      {/* </a> */}
+      <div className='footer__header'>
+        <div className= 'footer__header--text'>
+          <h1>Created By:</h1>
+          <h1>Tyler Ruby</h1>
+        </div>
+        <a className='footer__link' href='https://github.com/TylerRuby0821'>
+          <img className='footer__image' src= {github} alt='GitHub' height={50} width={50}></img>
+        </a>
+        <a className='footer__link' href='https://www.linkedin.com/in/tyler-ruby-b700161ba/'>
+          <img className='footer__image' src= {linkedIn} alt='LinkedIn' height={50} width={50}></img>
+        </a>
+        <a className='footer__link' href='https://tylerruby0821.github.io/'>
+          <div className='portfolio'>P</div>
+        </a>
+      </div>
     </div>
   )
 }
